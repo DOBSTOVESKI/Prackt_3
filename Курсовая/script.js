96ce9dc84b1da2d95c44066e4bf575a193917adc
Шадрин Егор <116781558+DOBSTOVESKI@users.noreply.github.com>
2023-02-11 18:53:53 +0700

    Add files via upload

--- Курсовая/script.js
+++ Курсовая/script.js
@@ -2,7 +2,9 @@ alert("ГБ ПОУ ТУЭТТ ИСП-1-20\nКурсовая работа\nШад
 alert('Нажмите "OK" для запуска');
 var one2frie;
 var hear;
+var hear2;
 var side;
+var tick=0;
 function l() {
     var q = document.getElementById('gun');
     if (parseInt(q.style.left) > 0) {        
@@ -16,9 +18,24 @@ function r() {
     if (parseInt(q.style.left) < window.screen.availWidth-100) {
         q.style.left = ((parseInt(q.style.left) + 70) + 'px');
         console.log(q.style.left);
-        her = q.style.left;
+        hear = q.style.left;
     }
 }
+function s(){
+    var w = document.getElementById('projectile');
+    var timerId = setInterval(function(){
+    w.style.top = ((parseInt(w.style.top) - 10) + 'px');
+    w.style.opacity = (parseInt(w.style.opacity) + 1);
+    console.log(w.style.top);
+    }, 10);
+    hear2=w.style.top;
+    setTimeout(function() {
+        clearInterval(timerId);
+        w.style.top = ((parseInt(w.style.top)+1000)+'px');
+        w.style.opacity = (parseInt(w.style.opacity)-100);
+      }, 1000);
+}
+
 var timerId = setInterval(() => spawnPlane(), 2000);
 function fluyPlaneR0(){
     
